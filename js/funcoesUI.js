af2ec1a88160c5f9ff7182da96dcbbccf84a8242
Bruno Costa <abscosta@yahoo.com.br>
2020-09-18 12:07:46 -0300

    Implementado a funcionalidade 'Câmbio' e melhorado interface

--- js/funcoesUI.js
+++ js/funcoesUI.js
@@ -1,3 +1,74 @@
+ function conversor(){
+      var Vdolar = 0;     
+      var Vcot = 0; 
+        //acessar o que foi digitado
+        var Vdolar = document.getElementById('dolares').value;
+        var Vcot = document.getElementById('cot').value;
+        
+        //converter de string para float
+        Vdolar = parseFloat(Vdolar);
+        Vcot = parseFloat(Vcot);
+        var Vfinal
+        Vfinal = Vdolar * Vcot
+        var final = Vfinal.toFixed(2) 
+        document.getElementById('result').innerHTML='O valor convertido para Moeda Real: R$ '+final
+      }
+
+function displayStuff(text) {
+ let msgInicial = document.getElementById("msgInicial")
+
+  if(text === "home")
+  {
+    document.getElementById("home").style.display = "block";
+    msgInicial.innerHTML = "Olá, seja bem vindo à Situação de Aprendizagem 2"
+  }
+  else {
+    document.getElementById("home").style.display = "none";
+    
+  }
+
+  if(text === "idade")
+  {
+    document.getElementById("idade").style.display = "block";
+    msgInicial.innerHTML = "Cálculo da idade a partir do ano de nascimento inserido pelo usuário"
+  }
+  else {
+    document.getElementById("idade").style.display = "none";
+    
+  }
+  
+  if(text === "cambio")
+  {
+    document.getElementById("cambio").style.display = "block";
+    conversor()
+    msgInicial.innerHTML = "Cálculo do valor em reais a partir de um valor em dólar e cotação inserida pelo usuário"
+
+  }
+  else {
+    document.getElementById("cambio").style.display = "none";
+  }
+  
+  if(text === "dia")
+  {
+    document.getElementById("dia").style.display = "block";
+    msgInicial.innerHTML = "Exibir o dia da semana a partir de uma data inserida pelo usuário"
+  }
+  else {
+    document.getElementById("dia").style.display = "none";
+  }
+  
+  if(text === "desconto")
+  {
+    document.getElementById("desconto").style.display = "block";
+    msgInicial.innerHTML = "Calcular  o  resultado  a  partir  de  um  valor  base  e  o percentual de desconto indicados pelo usuário"
+  }
+  else {
+    document.getElementById("desconto").style.display = "none";
+  }
+}
+  
+
+
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
