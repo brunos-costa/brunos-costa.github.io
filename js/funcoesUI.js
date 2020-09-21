e69956f522f02d5457c506757a1e67b23710b4e4
Bruno Costa <abscosta@yahoo.com.br>
2020-09-21 08:30:26 -0300

    Adicionada a funcionalidade 'Verificar Dia da semana'

--- js/funcoesUI.js
+++ js/funcoesUI.js
@@ -1,4 +1,5 @@
- function conversor(event){
+// Funcionalidade 2
+function conversor(event){
 	event.preventDefault()
 
 	var Vdolar = 0;     
@@ -31,6 +32,140 @@
 
 }
 
+// Funcionalidade 3
+document.getElementById('form-day').addEventListener('submit', function(e)
+{
+  e.preventDefault();
+  checkDayofWeek();
+  
+});
+
+function checkDayofWeek()
+{
+
+  document.getElementById("showText").classList.remove("card-panel");
+  document.getElementById("showText").classList.remove("teal");
+  let showDay;
+  let errorText = "";
+  let correctDate = true;
+  let leapYear = false;
+  let day = document.getElementById("day").value;
+  let month = document.getElementById("month").value;
+  let year = document.getElementById("year").value;
+
+
+  if(year%4 ==0)
+  {
+    leapYear=true;
+  }
+
+  if(month>12 || month<1)
+  {
+    correctDate = false;
+    errorText = "Mês inexistente."
+  }
+  if(month == 2)
+  {
+    if(leapYear)
+    {
+      if(day>29 || day <1)
+        correctDate = false;
+        errorText = errorText + "\nDia inexistente.";
+    }
+    else
+    {
+      if(day>28 || day <1)
+        correctDate = false;
+        errorText = errorText + "\nDia inexistente.";
+    }
+
+  }
+  switch(month)
+  {
+    case '2':
+      break;
+    case '4':
+    case '6':
+    case '9':
+    case '11':
+      {
+        if(day>30 || day<1)
+          correctDate = false;
+          errorText = errorText + "\nDia inexistente.";
+        break;
+      }
+    default:
+      {
+        
+        if(day>31 || day<1)
+          correctDate = false;
+          errorText = errorText + "\nDia inexistente.";
+        break;
+      }
+
+  }
+  
+    let date = new Date(year,month-1, day);
+    let dayOfWeek =date.getDay();
+    if(correctDate)
+    {
+      switch(dayOfWeek)
+      {
+        case 0:
+          {
+            showDay = "Domingo";
+            break;
+          }
+        case 1:
+          {
+            showDay = "Segunda-Feira";
+            break;
+          }
+        case 2:
+          {
+            showDay = "Terça-Feira";
+            break;
+          }
+        case 3:
+          {
+            showDay = "Quarta-Feira";
+            break;
+          }
+        case 3:
+          {
+            showDay = "Quarta-Feira";
+            break;
+          }
+        case 4:
+          {
+            showDay = "Quinta-Feira";
+            break;
+          }
+        case 5:
+          {
+            showDay = "Sexta-Feira";
+            break;
+          }
+        case 6:
+          {
+            showDay = "Sábado";
+            break;
+          }
+      }
+      document.getElementById("showText").innerHTML = "Esse dia é "+ showDay;
+      //document.getElementById("showText").classList.add("card-panel");
+      //document.getElementById("showText").classList.add("teal");
+    }
+    else
+    {
+      document.getElementById("showText").innerHTML = errorText
+      //document.getElementById("showText").classList.add("card-panel");
+      //document.getElementById("showText").classList.add("red");
+    }
+    
+}
+
+// Funcionalidade 4
 function calcdesc(event) {
 	event.preventDefault()
 
@@ -63,6 +198,10 @@ function calcdesc(event) {
 
 
 }
+
+
+
+
 function displayStuff(text) {
  let msgInicial = document.getElementById("msgInicial")
 
